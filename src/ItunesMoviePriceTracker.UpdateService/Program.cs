1f248c0b792e79800a2056a4df879556033d05e7
mib71 <mikael.barnholdt@hotmail.com>
2026-03-28 09:40:59 +0100

    chore: remove hardcoded currency suffix and add console logging to UpdateService

--- src/ItunesMoviePriceTracker.UpdateService/Program.cs
+++ src/ItunesMoviePriceTracker.UpdateService/Program.cs
@@ -9,11 +9,21 @@ Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .MinimumLevel.Override("Microsoft.EntityFrameworkCore", Serilog.Events.LogEventLevel.Warning)
     .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", Serilog.Events.LogEventLevel.Warning)
+    .WriteTo.Console()
     .WriteTo.File(
         path: "logs/log-.txt",
         rollingInterval: RollingInterval.Day,
-        retainedFileCountLimit: 30)
+        retainedFileCountLimit: 30,
+        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning)
+    .WriteTo.File(
+        path: "logs/app-.txt",
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 7,
+        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
     .CreateLogger();
+
+Log.Information("ItunesMoviePriceTracker.UpdateService v{Version} starting up",
+    typeof(Program).Assembly.GetName().Version);
 
 var host = Host.CreateDefaultBuilder(args)
     .UseSerilog()
@@ -38,5 +48,8 @@ await host.Services.ApplyMigrationsAsync();
 
 var priceUpdateService = host.Services.GetRequiredService<IPriceUpdateService>();
 var result = await priceUpdateService.CheckForPriceUpdateAsync();
+
+Log.Information("ItunesMoviePriceTracker.UpdateService shutting down");
+Log.CloseAndFlush();
 
 Console.WriteLine(result);
